7139f120eef0ca7b8398bbac8f9ced227e84d367
Eslam01Elgmaal <Eslam.Plutaki@gmail.com>
2026-07-13 18:48:01 +0300

    building business logic

--- GamePlay/admin.py
+++ GamePlay/admin.py
@@ -1,4 +1,29 @@
 from django.contrib import admin
-from .models import FootballPlayer
+from .models import FootballPlayer, Game, Pick
 
-admin.site.register(FootballPlayer)
+
+
+
+@admin.register(FootballPlayer)
+
+class FootballPlayerAdmin(admin.ModelAdmin):
+    list_display = ("name", "position")
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = (
+        "player_one_name",
+        "player_two_name",
+        "game_mode",
+        "created_at",
+    )
+
+@admin.register(Pick)
+class PickAdmin(admin.ModelAdmin):
+    list_display = (
+        "game",
+        "pick_owner",
+        "visible_player",
+        "hidden_player",
+        "is_visible_selected",
+    )
--- GamePlay/models.py
+++ GamePlay/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Turn(models.TextChoices):
+    PLAYER_ONE = "P1", "Player One"
+    PLAYER_TWO = "P2", "Player Two"
+
+
+class GameMode(models.TextChoices):
+    SINGLE = "S", "Single"
+    LOCAL = "L", "Local"
 
 class Position(models.TextChoices):
     GK = "GK", "GK"
@@ -17,14 +25,26 @@ class Position(models.TextChoices):
 class FootballPlayer(models.Model):
     name = models.CharField( max_length=50)
     position = models.CharField(max_length=3, choices=Position.choices )
-    imge = models.ImageField(upload_to='players/')
+    image = models.ImageField(upload_to='players/')
 
     def __str__(self):
         return self.name
 
 class Game(models.Model):
-    pass
-
+    player_one_name = models.CharField( max_length= 50)
+    player_two_name = models.CharField( max_length= 50)
+    game_mode = models.CharField(max_length=1, choices=GameMode.choices )
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.player_one_name} vs {self.player_two_name}"
 
 class Pick(models.Model):
-    pass
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="picks")
+    pick_owner = models.CharField(max_length=2, choices=Turn.choices)
+    hidden_player = models.ForeignKey(FootballPlayer, on_delete=models.CASCADE, related_name="hidden_picks")
+    visible_player = models.ForeignKey(FootballPlayer, on_delete=models.CASCADE, related_name="visible_picks")
+    is_visible_selected = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.game} - {self.pick_owner}"
+    
